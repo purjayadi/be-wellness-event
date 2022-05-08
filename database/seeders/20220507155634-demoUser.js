d6411b0d56c74b84cbe8f37bafe7d06bc7151a14
purjayadi <purjayadi@gmail.com>
2022-05-08 15:33:07 +0800

    remove faker from seeder

--- database/seeders/20220507155634-demoUser.js
+++ database/seeders/20220507155634-demoUser.js
@@ -1,6 +1,5 @@
 'use strict';
 const bcrypt = require('bcrypt');
-const { faker } = require('@faker-js/faker');
 
 function hashPassword (password) {
   return bcrypt.hashSync(password, bcrypt.genSaltSync(10));
@@ -10,7 +9,7 @@ module.exports = {
   async up (queryInterface) {
     const users = [
       {
-        id: faker.datatype.uuid(),
+        id: 'b55738f4-a3c1-4705-86b1-5c9f8c251367',
         username: 'hr',
         password: hashPassword('password'),
         name: 'HR',
@@ -19,7 +18,7 @@ module.exports = {
         updatedAt: new Date()
       },
       {
-        id: faker.datatype.uuid(),
+        id: '069600b0-1c86-4faa-8f04-a72bc38e45ec',
         username: 'hr2',
         password: hashPassword('password'),
         name: 'HR 2',
@@ -28,7 +27,7 @@ module.exports = {
         updatedAt: new Date()
       },
       {
-        id: faker.datatype.uuid(),
+        id: 'ef9ea0a9-99dc-4c75-b76f-285743084f76',
         username: 'vendor',
         password: hashPassword('password'),
         name: 'Vendor',
@@ -37,7 +36,7 @@ module.exports = {
         updatedAt: new Date()
       },
       {
-        id: faker.datatype.uuid(),
+        id: '8d6e3d2d-6225-421c-a051-46e473510069',
         username: 'vendor2',
         password: hashPassword('password'),
         name: 'Venor 2',
